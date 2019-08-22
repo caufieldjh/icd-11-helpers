2c2147ccb53b4dc5bb690440a461f7cd6cc65870
Harry Caufield <j.harry.caufield@gmail.com>
2019-08-22 12:13:55 -0700

    Richer output - now with original node URIs and chapters

--- parse_icd11mms_tab.py
+++ parse_icd11mms_tab.py
@@ -10,7 +10,7 @@ from the same directory as the script, provided as a command line
 argument.
 It outputs a tsv file of the following format:
 
-CodeA	TitleA	is_a	CodeB	TitleB
+URI_A	CodeA	TitleA	ChapterA	is_a	URI_A	CodeB	TitleB	ChapterB
 
 where is_a relations are defined by the hierarchy defined by the input.
 Residual children ("other" and "unspecified") are included.
@@ -32,20 +32,33 @@ args = parser.parse_args()
 
 ## Functions
 def parse(infile):
-	'''Store the parsed data'''
+	'''Store the parsed data.
+	A node_count is assigned internally to keep track of where
+	we are in the file, but nodes are also assigned their linearization
+	unique release ID (URI) as well as their plaintext
+	chapter membership.'''
 	
 	infile.readline() #Skip the header
 	
 	all_nodes = {}
 	
-	node_id = -1
+	node_count = -1
 	previous_level = 0
-	most_recent_id_at_level = {} #levels are keys, node_id is value
+	most_recent_id_at_level = {} #levels are keys, node_count is value
 	
 	for line in infile:
-		node_id = node_id +1
+		node_count = node_count +1
 		
 		splitline = line.split("\t")
+		
+		uri = (splitline[1].split("/"))[-1]
+		if uri in ["other","unspecified"]:
+			cleanuri = (splitline[1].split("/"))[-2] + "/" + uri
+		else:
+			cleanuri = uri
+		
+		chapterno = splitline[9]
+		
 		code = splitline[2]
 		title = splitline[4]
 		
@@ -62,20 +75,20 @@ def parse(infile):
 				cleantitle = cleantitle + char
 				
 		cleantitle = cleantitle.lstrip()
-		code_and_title = [code,cleantitle]
 		
 		#Now let's figure out what the parent is.
-		#Keep track of the node_id of the previous level was
+		#Keep track of the node_count of the previous level was
 		if level == 0:
 			parent = "None"
 		else:
 			parent = most_recent_id_at_level[level - 1]
 		
-		all_nodes[node_id] = [code_and_title, parent] #parent is a node_id
+		all_nodes[node_count] = {"uri":cleanuri,"code":code,"title":cleantitle,
+								"chapter":chapterno,"parent":parent} #parent is a node_count
 		
 		previous_level = level
 		
-		most_recent_id_at_level[level] = node_id
+		most_recent_id_at_level[level] = node_count
 		
 	return all_nodes
 		
@@ -84,22 +97,28 @@ def write_out(node_dict):
 	
 	with open("outfile.tsv", "w") as outfile:
 		for node in node_dict:
-			parent_id = node_dict[node][1]
-			codeA = node_dict[node][0][0]
-			titleA = node_dict[node][0][1]
+			parent_id = node_dict[node]["parent"]
+			uriA = node_dict[node]["uri"]
+			codeA = node_dict[node]["code"]
+			titleA = node_dict[node]["title"]
+			chapterA = node_dict[node]["chapter"]
 				
 			if parent_id == "None":
 				pass
 			else:
-				codeB = node_dict[parent_id][0][0]
-				titleB = node_dict[parent_id][0][1]
+				uriB = node_dict[parent_id]["uri"]
+				codeB = node_dict[parent_id]["code"]
+				titleB = node_dict[parent_id]["title"]
+				chapterB = node_dict[parent_id]["chapter"]
 				
 				if codeA == "":
 					codeA = "NA"
 				if codeB == "":
 					codeB = "NA"
 				
-				out_string = "%s\t%s\tis_a\t%s\t%s\n" % (codeA, titleA, codeB, titleB)
+				out_string = "%s\t%s\t%s\t%s\tis_a\t%s\t%s\t%s\t%s\t\n" % (uriA, codeA, 
+																titleA, chapterA, uriB,
+																codeB, titleB, chapterB)
 				outfile.write(out_string)
 	
 ## Main
